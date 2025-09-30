0ef206bc622faf5d8c133ccae30b21702c151a04
zuki <wxq839190594@gmail.com>
2025-09-30 22:06:55 +1300

    Add Vercel Analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@
 import { createRoot } from "react-dom/client";
 import AppRouter from "./AppRouter.tsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react"
 
-createRoot(document.getElementById("root")!).render(<AppRouter />);
+createRoot(document.getElementById("root")!).render(
+  <>
+  <AppRouter />
+  <Analytics />
+  </>
+);
   
